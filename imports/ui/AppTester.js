f6fa76ad28b9286f7a0dbccbde48279aa09e5d98
sneiderV <e.velandia2164@uniandes.edu.co>
2018-03-26 16:15:00 -0500

    Agrego vistas basicas del Tester
    agrego la navegacion basica del tester y agrego popUp del comentario que hace el Tester

--- imports/ui/AppTester.js
+++ imports/ui/AppTester.js
@@ -1,10 +1,22 @@
 
 import React, { Component } from 'react';
+import ProyectoTester from "./ProyectoTester.js";
 
 export default class AppTester extends Component {
-	render() {
-		return (
-			<div> ERES UN TESTER </div>
-		);
-	}
+    render() {
+        return (
+        <div> ERES UN TESTER 
+            <button id="historial" >Historial</button>
+            <div>
+            {this.props.proyectos.map((proyecto)=>{
+                return <ProyectoTester 
+                key={proyecto._id}
+                proyecto={proyecto}
+                />
+                })
+            }
+            </div>
+        </div>
+        );
+    }
 }
