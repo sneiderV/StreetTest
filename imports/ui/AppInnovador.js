1538373989de8fff5669fe9faace302ac0ef5f10
sneiderV <e.velandia2164@uniandes.edu.co>
2018-03-26 15:38:34 -0500

    subo popUp de frmulario para crear proyecto

--- imports/ui/AppInnovador.js
+++ imports/ui/AppInnovador.js
@@ -1,30 +1,59 @@
 import React, { Component } from 'react';
 import ProyectoInnovador from "./ProyectoInnovador.js";
+import swal from 'sweetalert';
+import "./swal-forms.css"
+import "./swal-forms.js"
+import "./sweet-alert.js"
+import "./sweet-alert.css"
 
 /**
  * Componente que representa la pantalla "AppInnovador"
  * props: proyectos
  **/
-export default class AppInnovador extends Component {
+ export default class AppInnovador extends Component {
 
+ 	pop(){
+ 		    swal.withForm({
+            title: 'Crear Proyecto',
+            text: 'Porfavor ingrese los datos para crear su proyecto.',
+            showCancelButton: true,
+            confirmButtonColor: '#DD6B55',
+            confirmButtonText: 'Crear',
+            // closeOnConfirm: true,
+            formFields: [
+            { id: 'nonmbre', placeholder:'Nombre del Proyecto', required: true },
+            { id: 'url', placeholder:'Ingresa la URL de tu proyecto', required: true },
+            { id: 'tema', placeholder:'Tema general', required: true },
+            { id: 'descripcion', placeholder:'Ingrese una breve descripción', required: true },
+            { id: 'tareas', placeholder:'Agregue las tareas que desea que se realicen', required: true }
+            ]
+            
+        }, function(isConfirm) {
+            // Aqui estan los datos de los usuarios
+            if (isConfirm === true) {
+                swal("Buen trabajo!", "Tu proyecto fue creado, espara por los comentarios", "success");
+                console.log(this.swalForm) // lanza un objeto con los parametros que fueron ingresados por el usuario
+            }
+        });
+ 	}
 
+ 	render() {
+ 		return (
+ 			<div> 
+ 			ERES UN INNOVADOR 
+ 			<h3>Proyectos (de prueba)</h3>
+ 			<button id="crear" onClick={this.pop}>Crear</button>
+ 			<div>
+ 			{this.props.proyectos.map((proyecto)=>{
+ 				return <ProyectoInnovador 
+ 				key={proyecto._id}
+ 				proyecto={proyecto}
+ 				/>
+ 			})
+ 		}
+ 		</div>
+ 		</div>
 
-	render() {
-		return (
-			<div> 
-				ERES UN INNOVADOR 
-					<h3>Proyectos (de prueba)</h3>
-				<div>
-					{this.props.proyectos.map((proyecto)=>{
-					 	return <ProyectoInnovador 
-					 		key={proyecto._id}
-					 		proyecto={proyecto}
-					 	/>
-					})
-						}
-				</div>
-			</div>
-			
-		);
-	}
-}
+ 		);
+ 	}
+ }
