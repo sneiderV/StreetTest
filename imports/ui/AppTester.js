eafc88d06ee32f2c86520b244dcff8b5378ff59b
sneiderV <e.velandia2164@uniandes.edu.co>
2018-03-26 18:02:57 -0500

    Agrego la vista del historial para el tester

--- imports/ui/AppTester.js
+++ imports/ui/AppTester.js
@@ -1,12 +1,20 @@
 
 import React, { Component } from 'react';
 import ProyectoTester from "./ProyectoTester.js";
+import {mount} from "react-mounter";
+import historialTester from "./historialTester"
 
 export default class AppTester extends Component {
+    
+    irHistorial(){
+        mount(historialTester)
+    }
+
+
     render() {
         return (
         <div> ERES UN TESTER 
-            <button id="historial" >Historial</button>
+            <button id="historial" onClick={this.irHistorial} >Historial</button>
             <div>
             {this.props.proyectos.map((proyecto)=>{
                 return <ProyectoTester 
---
+++ imports/ui/historialTester.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class historialTester extends Component {
+
+
+    render() {
+        return (
+            <div>
+            <h1>Nombre: usuario</h1>
+            <h3>Total de puntos: </h3>
+
+            <h5>Estos son los puntos acumulados en StreetTest: </h5>
+
+
+            </div>
+            );
+    }
+}
