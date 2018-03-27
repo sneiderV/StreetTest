6d2e4856f9f4145b18eb817b7db235ec0bfd0234
Fabio Lopez <fa.lopez10@uniandes.edu.co>
2018-03-27 00:03:54 -0500

    integracion de cuentas

--- client/main.js
+++ client/main.js
@@ -2,11 +2,13 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 
+import AccountsUI from "../imports/ui/AccountsUI.js"
 // import "../imports/startup/accounts-config.js"
 import App from "../imports/ui/App.js";
 
 Meteor.startup(()=>{
 	{
+		render(<AccountsUI />, document.getElementById("login"));
 		render(<App />, document.getElementById("react-root"));
 	}
 });
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -5,6 +5,9 @@ import AppTester from "./AppTester.js";
 import "./css/app.css"
 import { withTracker } from "meteor/react-meteor-data";
 
+//Manejo de sign in/up
+import AccountsUI from "./AccountsUI";
+
 //collection
 import { Proyectos } from "../api/proyectos.js";
 
@@ -14,30 +17,44 @@ import { Proyectos } from "../api/proyectos.js";
  **/
 export class App extends Component {
 
+	ingreseCuentaAlert(){
+		alert("Debe ingresar a su cuenta para realizar esta acción");
+	}
+
 	cambiarPantallaInnovador(){
+
+		console.log(JSON.stringify(Meteor.user()))
 		 // FlowRouter.go("/appinnovador");
-		mount(AppInnovador,{proyectos: this.props.proyectos})
+		if(this.props.currentUser)
+			mount(AppInnovador,{proyectos: this.props.proyectos,
+													ingreseCuentaAlert: this.ingreseCuentaAlert})
+		else
+			this.ingreseCuentaAlert();
 	}
 
 	cambiarPantallaTester(){
 		 // FlowRouter.go("/appinnovador");
-		mount(AppTester,{proyectos: this.props.proyectos})
+		 if(this.props.currentUser)
+			mount(AppTester,{proyectos: this.props.proyectos,
+													ingreseCuentaAlert: this.ingreseCuentaAlert})
+		else
+			this.ingreseCuentaAlert();
 	}
 
 	render() {
-		
 		return (
 			// se diseña parte comun s
 			<div>
 				<div>
-				<h1>Street Test</h1>
-				<h4>Quieres ingresar como:</h4>
-				{/*botones de cada uno llevan  a pantallas de login distinto, se debe pasr por parámetro el rol*/}
-				<center>
-				<button id="Innovador" onClick={this.cambiarPantallaInnovador.bind(this)}>Innovador</button>
-				<span>                                                                      </span>
-				<button id="Tester" onClick={this.cambiarPantallaTester.bind(this)}>Tester</button>
-				</center>
+					<h1>Street Test</h1>
+					
+					<h4>Quieres ingresar como:</h4>
+					{/*botones de cada uno llevan  a pantallas de login distinto, se debe pasr por parámetro el rol*/}
+					<center>
+					<button id="Innovador" onClick={this.cambiarPantallaInnovador.bind(this)}>Innovador</button>
+					<span>                                                                      </span>
+					<button id="Tester" onClick={this.cambiarPantallaTester.bind(this)}>Tester</button>
+					</center>
 				</div>
 			</div>
 			);
